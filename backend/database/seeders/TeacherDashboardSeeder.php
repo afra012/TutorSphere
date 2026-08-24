85c8dea9559bfc2bed90e1144df86cf49a9d81c7
fahmida47 <fahmida.200547@gmail.com>
2026-08-25 02:02:23 +0600

    feat: add teacher dashboard seeder

---
+++ backend/database/seeders/TeacherDashboardSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\TeacherDashboard\Subject;
+use App\Models\TeacherDashboard\Language;
+
+class TeacherDashboardSeeder extends Seeder
+{
+    public function run(): void
+    {
+        /*
+        |--------------------------------------------------------------------------
+        | Subjects
+        |--------------------------------------------------------------------------
+        */
+
+        $subjects = [
+            'Mathematics',
+            'Physics',
+            'Chemistry',
+            'English',
+            'Biology',
+            'ICT',
+            'Computer Science',
+            'Bangla',
+            'Accounting',
+            'Economics',
+        ];
+
+        foreach ($subjects as $subject) {
+            Subject::firstOrCreate([
+                'subject_name' => $subject,
+            ]);
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | Languages
+        |--------------------------------------------------------------------------
+        */
+
+        $languages = [
+            'English',
+            'Bangla',
+            'Hindi',
+            'Arabic',
+            'Spanish',
+            'French',
+        ];
+
+        foreach ($languages as $language) {
+            Language::firstOrCreate([
+                'language_name' => $language,
+            ]);
+        }
+    }
+}
