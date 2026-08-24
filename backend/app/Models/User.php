96a13b900e2e1499a3162c92d0426ad6d41e0614
fahmida47 <fahmida.200547@gmail.com>
2026-08-25 02:00:20 +0600

    feat: update user model for teacher profile

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\TeacherDashboard\Language;
+use App\Models\TeacherDashboard\Subject;
+use App\Models\TeacherDashboard\TeacherProfile;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -29,5 +32,32 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function teacherProfile()
+    {
+        return $this->hasOne(TeacherProfile::class, 'user_id');
+    }
+
+    public function teachingSubjects()
+    {
+        return $this->hasManyThrough(
+            Subject::class,
+            TeacherProfile::class,
+            'user_id',
+            'id',
+            'id',
+            'id'
+        );
+    }
+
+    public function teachingLanguages()
+    {
+        return $this->belongsToMany(
+            Language::class,
+            'teacher_languages',
+            'teacher_id',
+            'language_id'
+        )->withTimestamps();
     }
 }
