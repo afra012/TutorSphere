b1703493099781a28d99a0851822b87fb81c3606
fahmida47 <fahmida.200547@gmail.com>
2026-09-22 00:30:33 +0600

    feat: add review status migration

---
+++ backend/database/migrations/2026_09_20_202043_add_status_to_reviews_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->enum('status', [
+                'pending',
+                'approved',
+                'rejected'
+            ])->default('pending')->after('review_text');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
