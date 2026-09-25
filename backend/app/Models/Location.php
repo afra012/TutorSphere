512a55a9f0b1bf53109ebd869bc3cb4cc3128504
fahmida47 <fahmida.200547@gmail.com>
2026-09-25 21:00:05 +0600

    add location model

---
+++ backend/app/Models/Location.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Location extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'latitude',
+        'longitude',
+        'address',
+    ];
+
+    protected $casts = [
+        'latitude' => 'float',
+        'longitude' => 'float',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(
+            User::class,
+            'user_id'
+        );
+    }
+}
